5ee23382a2d419eba1822a57ea70c8ae984c1f6e
Sasa <safdiesantiago98@gmail.com>
2019-05-31 11:36:10 -0300

    Auto stash before merge of "master" and "origin/master"

--- 4buscador.js
+++ 4buscador.js
@@ -6,24 +6,21 @@ var query = URLParams.get('buscador');
 
 console.log(query);
 
-
-  var url = "https://api.giphy.com/v1/gifs/search?api_key=oDTxfeJUVjbbzW3BQmumHLRIMuDsukrL&q="+ query +"&limit=25&offset=0&rating=G&lang=en"
+  var url = "https://api.themoviedb.org/3/search/movie?api_key=0f61f9e045cc4f90eaffcb0e1aff08fb&language=en-US&query=" + query + "&page=1&include_adult=false&limit=25&offset=0&rating=G&lang=en"
     fetch(url)
       .then(function(respuesta) {
         return respuesta.json()
       })
       .then(function(resultado) {
-        console.log("uhrjdfk");
         console.log(resultado);
-        console.log(resultado.data);
-     //   var arrayDeGifs = resultado.data si quiero en ves de resultado.data
-        // document.querySelector(".avatar-user").src = resultado.data
-        for (var i = 0; i < resultado.data.length; i++) {
-         var id = resultado.data[i].id
-         var title = resultado.data[i].title
-         var url = resultado.data[i].images.original.url
+       var peliculas = resultado.results
+        //document.querySelector(".avatar-user").src = resultado.data
+       for (var i = 0; i < peliculas.length; i++) {
+        //var id = resultado.data[i].id
+         var title = peliculas[i].title
+         //var url = resultado.data[i].images.original.url
 
-         document.querySelector("h1").innerHTML += "<p><a href='detalle.html?id=" + id + "'>" + title + "</p>";
+        document.querySelector("h1").innerHTML += "<p><a href='detalle.html?id='>" + title + "</p>";
          document.querySelector("h1").innerHTML += "<img src=" + url + ">"
 
 
@@ -33,3 +30,4 @@ console.log(query);
       .catch(function(error) {
         console.log("Error: " + error);
       })
+}
