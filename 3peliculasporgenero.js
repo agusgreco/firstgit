d018626ad78e1aaf143a76ac98be1c22be18fc3e
agus greco <grecoagustina18@gmail.com>
2019-06-04 13:12:57 -0300

    ndkx

--- 3peliculasporgenero.js
+++ 3peliculasporgenero.js
@@ -35,15 +35,71 @@ console.log(id);
 
 
       var article = ""
-      article += "<article>"
-        article += "<a class='genretitle' href='5detallepelicula.html?id=" + id + "'> <p> " + title + " </p>"
-        article += "<img src='" + mainurl+ poster + "' style='width: 300px;' > </a>"
+      article += "<article class='slideshow-container'>"
+        article += "<div class='mySlides fade'>"
+         article +=   "<a class='genretitle' href='5detallepelicula.html?id=" + id + "'>"
+         article += "<img src='" + mainurl+ poster + "' style='width: 300px;' >"
+         article += "<div> <p class='thispara'> " + title + " </p> </div> </a>"
+         article +=  "<a class='prev' onclick='plusSlides(-1)'>&#10094;</a> <a class='next' onclick='plusSlides(1)'>&#10095;</a>"
+
+        article += "</div>"
+
+         article += "<div style='text-align:center'>"
+         article += "<span class='dot' onclick='currentSlide(i)'> </span>"
+       article += "</div>"
+
       article += "</article>"
       document.querySelector("main").innerHTML += article
 
+//DESDE ACA
+
+      var slideIndex = 1;
+      showSlides(slideIndex);
+
+      // Next/previous controls
+      function plusSlides(n) {
+        showSlides(slideIndex += n);
+      }
+
+      // Thumbnail image controls
+      function currentSlide(n) {
+        showSlides(slideIndex = n);
+      }
+
+      // function showSlides(n) {
+      //   var i;
+      //   var slides = document.getElementsByClassName("mySlides");
+      //   var dots = document.getElementsByClassName("dot");
+      //   if (n > slides.length) {slideIndex = 1}
+      //   if (n < 1) {slideIndex = slides.length}
+      //   for (i = 0; i < slides.length; i++) {
+      //       slides[i].style.display = "none";
+      //   }
+      //   for (i = 0; i < dots.length; i++) {
+      //       dots[i].className = dots[i].className.replace(" active", "");
+      //   }
+      //   slides[slideIndex-1].style.display = "block";
+      //   dots[slideIndex-1].className += " active";
+      // }
 
 
+      var slideIndex = 0;
+      showSlides();
 
+      function showSlides() {
+        var i;
+        var slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+      }
+
+
+///HASTA ACA
      }
    })
    .catch(function(error) {
