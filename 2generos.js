256284d8f756ce1421e3148bf932da79c9fcb3e6
agus greco <grecoagustina18@gmail.com>
2019-06-11 17:13:56 -0300

    uwelebjdnx,

--- 2generos.js
+++ 2generos.js
@@ -16,10 +16,49 @@ window.onload = function(){
      //  console.log(classs)
      //
       // document.querySelector(classs).innerHTML += "<p><a href='3peliculasporgenero.html?id=" + resultado.genres[i].id + "'>" + resultado.genres[i].name + "</p>";
-      for (var i = 0; i < resultado.genres.length; i++) {
-        document.querySelector(".generos").innerHTML += "<div class='gene'><p><a class='genreclasss' href='3peliculasporgenero.html?id=" + resultado.genres[i].id + "&name=" + resultado.genres[i].name + "'>" + resultado.genres[i].name + "</p></div>";
-
+      genos = ""
+      for (var i = 0; i < 5; i++) {
+        genos += "<div class='gene'>"
+        genos += "<p>"
+        genos += "<a class='genreclasss' href='3peliculasporgenero.html?id=" + resultado.genres[i].id + "&name=" + resultado.genres[i].name + "'>"
+        genos += resultado.genres[i].name + "</p>"
+        genos += "</a></div>"
       }
+      // document.querySelector(".generos").innerHTML = genos
+
+      genosdos = ""
+      for (var i = 5; i < resultado.genres.length; i++) {
+        genosdos += "<div class='gene'>"
+        genosdos +=   "<p>"
+        genosdos +=   "<a class='genreclasss' href='3peliculasporgenero.html?id=" + resultado.genres[i].id + "&name=" + resultado.genres[i].name + "'>"
+        genosdos +=     resultado.genres[i].name + "</p>"
+        genosdos +=   "</a>"
+        genosdos += "</div>"
+      }
+      // document.querySelector(".generos").innerHTML += genosdos
+      console.log(genosdos);
+      // document.querySelector(".generos").innerHTML += "<div class='gene'><p><a class='genreclasss' href='3peliculasporgenero.html?id=" + resultado.genres[i].id + "&name=" + resultado.genres[i].name + "'>" + resultado.genres[i].name + "</p></div>";
+
+
+      var articlesuge = ""
+      articlesuge += genos
+      articlesuge += '<button id="mo" > VER MAS </button>'
+      articlesuge += "<article class='ocu ocultar'>"
+      articlesuge +=  genosdos
+      articlesuge += "</article>"
+      document.querySelector(".generos").innerHTML += articlesuge
+
+
+      // var ocultado = document.querySelector('.ocultar')
+
+      var bot = document.querySelector('#mo')
+      bot.addEventListener('click', mostrar)
+      function mostrar(){
+         document.querySelector('.ocu').classList.toggle('ocultar')
+      }
+
+     //  div += "</div>"
+     // document.querySelector(".generos").insertAdjacentHTML('beforeend', div)
 
      // }
    })
