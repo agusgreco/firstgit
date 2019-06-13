ab359cebeb23d6e0530efd530cb9ef2015347a9c
zoehamra <zoehamra@gmail.com>
2019-06-12 21:21:42 -0300

    Hice el login chiques

---
+++ login.js
@@ -0,0 +1,34 @@
+var theForm = document.querySelector("#Myform");
+theForm.onsubmit = function(event){
+  var boton = document.querySelector("#nom");
+  var boton2 = document.querySelector("#contra");
+  var boton3 = document.querySelector("#mail");
+
+
+  if(boton.value == "" || boton2.value == "" || boton3.value == ""){
+    event.preventDefault();
+    alert("che escribi bobo");
+  }else if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(boton3.value))){
+    event.preventDefault();
+    alert("You have entered an invalid email address!")
+  }else {
+    var name = boton.value;
+    var pass = boton2.value;
+    var mail = boton3.value;
+    localStorage.setItem("nombre",name);
+    localStorage.setItem("contrasenia",pass);
+    localStorage.setItem("mail", mail);
+    console.log(name);
+    console.log(localStorage);
+    var modal = document.querySelector("#modal-example");
+    modal.style.display = "none";
+    var login = document.querySelector("#Mylogin");
+    login.style.display = "none";
+    var nombre = localStorage.getItem("nombre");
+    var span = document.querySelector("#span");
+    span.innerText = nombre;
+    var user = document.querySelector("#user");
+    user.style.display = "inline-block";
+    
+  }
+}
